676b38d94a86497dc599d85dca44a92899fd881a
klhelker <57188023+klhelker@users.noreply.github.com>
2020-03-04 09:39:00 -0600

    created questions and ability to store input

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,49 +20,62 @@ var connection = mysql.createConnection({
     if (err) throw err;
     console.log("connected");
     queryAllItems();
-    itemId();
-    quantity();
+    
   });
   
   function queryAllItems() {
     connection.query("SELECT * FROM products", function(err, res) {
       if (err) throw err;
-      for (var i = 0; i < res.length; i++) {
-        console.log("item id = " + res[i].item_id + " | " + "product name = " + res[i].product_name + " | " + "department name = " + res[i].department_name + " | " + "price = $ " + res[i].price + " | " + "quantity = " + res[i].stock_quantity);
-      }
+      // for (var i = 0; i < res.length; i++) {
+      //   console.log("item id = " + res[i].item_id + " | " + "product name = " + res[i].product_name + " | " + "department name = " + res[i].department_name + " | " + "price = $ " + res[i].price + " | " + "quantity = " + res[i].stock_quantity);
+      // }
+      console.table(res)
       console.log("-----------------------------------");
+      itemId(res);
     });
+    
+    // quantity();
   }
 
-function itemId() {
+function itemId(results) {
+            
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].product_name);
+            }
+            console.log(choiceArray);
 //   // query the database for all items being auctioned
 //   connection.query("SELECT * FROM products", function(err, results) {
 //     if (err) throw err;
    inquirer
       .prompt([
         {
-          name: "item id",
+          name: "productName",
           type: "list",
-          choices: function() {
-            var choiceArray = [];
-            for (var i = 0; i < results.length; i++) {
-              choiceArray.push(results[i].item_name);
-            }
-            return choiceArray;
-          },
+          choices: choiceArray,
           message: "What is the item_id of the product you would like?"
-        }
+
+          },
+          {
+            name: "quantity",
+            type: "number",
+            message: "What quantity of the item you would like to buy?"
+
+          }
+        
+        ])
+        
         .then(function(answer) {
-          var query = "SELECT * FROM products";
-          connection.query(query, {item_id: answer.item_id}, function(err, res) {
-            for (var i = 0; i < res.length; i++) {
-              console.log("you chose: " + res[i].item_id);
-            }
-          
+
+          console.log(answer)
+          for(var i = 0; i < results.length; i++){
+              if (answer.productName === results[i].product_name){
+
+                  console.log(results[i]);
+              }
+          }
+
           });
-          
-        })
-      ])
     }
     function quantity() {
       //   // query the database for all items being auctioned
